a4ec2f7a26007601ca601bd302b6d523027ae186
cheezitlin <ethan.arnolddd12314@gmail.com>
2025-05-07 15:00:20 -0500

    Update instrgen.sh

--- instrgen.sh
+++ instrgen.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "i am too lazy to implement a sudo check, but this needs to be ran as sudo!"
-sleep 1
-echo "3..."
-sleep 1
-echo "2..."
-sleep 1
-echo "1..."
 sleep 0.25
+echo "the program will be copied in 5 seconds, this is so that if you change your mind, then do ctrl c or ctrl z."
+sleep 5
 echo "copying rgen to /usr/bin..."
 cp rgen /usr/bin/rgen
 sleep 0.25
