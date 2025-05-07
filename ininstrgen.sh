375333e75177a99a649a97b0d5eb0f1221705614
cheezitlin <ethan.arnolddd12314@gmail.com>
2025-05-07 15:03:37 -0500

    Create ininstrgen.sh
    uninstaller

---
+++ ininstrgen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "this script will uninstall rgen in 10 seconds, do ctrl z or ctrl c to cancel (suspend this program). run as sudo."
+sleep 10
+rm -f /usr/bin/rgen
+echo "done. exiting.."
+exit
